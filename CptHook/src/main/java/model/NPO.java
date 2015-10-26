2cb1e826f49457f08d18f91ae9ee824bf244a756
David Eichin <david@eich.in>
2015-10-26 16:57:50 +0100

    fixed problem with duplication of type attribute in json

--- CptHook/src/main/java/model/NPO.java
+++ CptHook/src/main/java/model/NPO.java
@@ -1,6 +1,7 @@
 package model;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeId;
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
@@ -9,12 +10,14 @@ import lombok.Setter;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME,
               include = JsonTypeInfo.As.PROPERTY,
+              visible = true,
               property = "type")
 @JsonSubTypes({
     @Type(value = Block.class, name = "block")
 })
 public class NPO {
 
+	@JsonTypeId
 	@Getter @Setter
 	private String type;
 	
