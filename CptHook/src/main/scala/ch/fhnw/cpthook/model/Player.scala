c3b0fd52b07b1010f8224054a646000d1297d185
David Eichin <david@eich.in>
2016-01-07 16:21:39 +0100

    removed debug output

--- CptHook/src/main/scala/ch/fhnw/cpthook/model/Player.scala
+++ CptHook/src/main/scala/ch/fhnw/cpthook/model/Player.scala
@@ -30,7 +30,6 @@ class Player(var position: Position) extends ContactUpdates {
   var stepAnimation: Integer = 0
   val mesh: IMesh = Player.createMesh(this)
   val walkingAnimation = (1 until 8).map { n => 
-    println(n)
     new ColorMapMaterial(Frame.create(getClass.getResource(s"../assets/step$n.png")).getTexture())
   }.toArray
   
