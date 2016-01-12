d21620e71a5edcc8d1f80997cf647a0562a6b8a7
David Eichin <david@eich.in>
2016-01-12 15:54:33 +0100

    changed colormap to texture

--- CptHook/src/main/scala/ch/fhnw/cpthook/model/Block.scala
+++ CptHook/src/main/scala/ch/fhnw/cpthook/model/Block.scala
@@ -17,47 +17,48 @@ import org.jbox2d.collision.shapes.PolygonShape
 import org.jbox2d.dynamics.BodyType
 import org.jbox2d.dynamics.World
 import org.jbox2d.dynamics.Body
+import ch.fhnw.ether.scene.mesh.material.Texture
 
-abstract class Block(var position: Position, var size: Size, var material: ColorMapMaterial) extends Entity {
+abstract class Block(var position: Position, var size: Size, var texture: Texture) extends Entity {
   def getFriction: Float
   def getRestitution: Float
   
   def linkBox2D(world: World): Unit = Block.createDefaultBox2D(world, this)
-  def toMesh(): ch.fhnw.ether.scene.mesh.IMesh = Block.createDefaultCube(material, position, size)
+  def toMesh(): ch.fhnw.ether.scene.mesh.IMesh = Block.createDefaultCube(texture, position, size)
 }
 
-class GrassBlock(position: Position, size: Size) extends Block(position, size, Block.GrassMaterial) {
+class GrassBlock(position: Position, size: Size) extends Block(position, size, Block.GrassTexture) {
   def getFriction = 0.2f
   def getRestitution = 0.1f
 }
 
-class DirtBlock(position: Position, size: Size) extends Block(position, size, Block.DirtMaterial) {
+class DirtBlock(position: Position, size: Size) extends Block(position, size, Block.DirtTexture) {
   def getFriction = 0.2f
   def getRestitution = 0.1f
 }
 
-class IceBlock(position: Position, size: Size) extends Block(position, size, Block.IceMaterial) {
+class IceBlock(position: Position, size: Size) extends Block(position, size, Block.IceTexture) {
   def getFriction = 0.2f
   def getRestitution = 0.1f
 }
 
-class LavaBlock(position: Position, size: Size) extends Block(position, size, Block.LavaMaterial) {
+class LavaBlock(position: Position, size: Size) extends Block(position, size, Block.LavaTexture) {
   def getFriction = 0.2f
   def getRestitution = 0.1f
 }
 
-class TargetBlock(position: Position, size: Size) extends Block(position, size, Block.TargetMaterial) {
+class TargetBlock(position: Position, size: Size) extends Block(position, size, Block.TargetTexture) {
   def getFriction = 0.2f
   def getRestitution = 0.1f
 }
 
 object Block {
   
-  val GrassMaterial = Entity.loadMaterial("../assets/grass.png")
-  val DirtMaterial = Entity.loadMaterial("../assets/dirt.png")
-  val IceMaterial = Entity.loadMaterial("../assets/ice.png")
-  val LavaMaterial = Entity.loadMaterial("../assets/lava.png")
-  val TargetMaterial = Entity.loadMaterial("../assets/target.png")
+  val GrassTexture = Entity.loadTexture("../assets/grass.png")
+  val DirtTexture = Entity.loadTexture("../assets/dirt.png")
+  val IceTexture = Entity.loadTexture("../assets/ice.png")
+  val LavaTexture = Entity.loadTexture("../assets/lava.png")
+  val TargetTexture = Entity.loadTexture("../assets/target.png")
 
   /**
    * Texture coordinates for default cube
@@ -72,9 +73,9 @@ object Block {
   /**
    * Creates a default cube with the given material.
    */
-  def createDefaultCube(material: ColorMapMaterial, position: Vec3, size: Vec3) : IMesh = {
+  def createDefaultCube(texture: Texture, position: Vec3, size: Vec3) : IMesh = {
     val g = DefaultGeometry.createVM(Primitive.TRIANGLES, MeshUtilities.UNIT_CUBE_TRIANGLES, textureCoordinates);
-    val mesh = new DefaultMesh(material, g, Queue.DEPTH)
+    val mesh = new DefaultMesh(new ColorMapMaterial(texture), g, Queue.DEPTH)
     mesh.setPosition(position add (size scale 0.5f))
     mesh.setTransform(Mat4 scale size)
     mesh
--- CptHook/src/main/scala/ch/fhnw/cpthook/model/Monster.scala
+++ CptHook/src/main/scala/ch/fhnw/cpthook/model/Monster.scala
@@ -18,31 +18,47 @@ import org.jbox2d.callbacks.ContactListener
 import ch.fhnw.cpthook.tools.ContactUpdates
 import org.jbox2d.dynamics.contacts.Contact
 import org.jbox2d.dynamics.Body
+import ch.fhnw.ether.scene.mesh.material.ColorMapMaterial
 
 class Monster(var position: Position) extends Entity with EntitiyUpdatable with EntityActivatable with ContactUpdates {
   
-  var animationStep = 0
   var mesh: IMesh = null
   var body: Body = null
   var leftSensor: Fixture = null
   var rightSensor: Fixture = null
+  
   var velocity: Float = -Monster.Speed
+  
+  var animationStep = 0
+  var timeOfAnimation: Double = 0.0
+  val animationStepTime: Double = 0.4
   
   def toMesh(): IMesh = Monster.toMesh(this)
   def linkBox2D(world: World): Unit = Monster.linkBox2d(this, world)
   
   def activate(gameContactListener: GameContactListener): Unit = {
     animationStep = 0
+    timeOfAnimation = 0
     velocity = -Monster.Speed
     gameContactListener.register(this, leftSensor)
     gameContactListener.register(this, rightSensor)
+    
   }
   
   def deactivate(): Unit = {
     mesh.setPosition(position.add(new Vec3(Monster.Width / 2f, Monster.Height / 2f, -0.5f)))
   }
   
-  def update(inputManager: InputManager,time: Double): Unit = {
+  def update(inputManager: InputManager, time: Double): Unit = {
+    
+    timeOfAnimation += time
+    
+    if (timeOfAnimation > animationStepTime) {
+      timeOfAnimation = 0
+      animationStep = (animationStep + 1) % Monster.AnimationFrames.length
+      mesh.getMaterial().asInstanceOf[ColorMapMaterial].setColorMap(Monster.AnimationFrames(animationStep))
+    }
+    
     val v = body.getLinearVelocity
     body.setLinearVelocity(new org.jbox2d.common.Vec2(velocity, v.y))
     val newPosition = new Vec3(body.getPosition.x, body.getPosition.y, -0.5f)
@@ -71,13 +87,13 @@ object Monster {
   val Speed = 1f
   val Width = 1f
   val Height = 1f
-  val AnimationFrames = (1 to 11).map { n => Entity.loadMaterial(s"../assets/monster_step$n.png") }.toArray
+  val AnimationFrames = (1 to 11).map { n => Entity.loadTexture(s"../assets/monster_step$n.png") }.toArray
   
   val Vertices = Entity.twoDimensionalPlane(Width, Height, 0f)
   val Geometry = DefaultGeometry.createVM(Primitive.TRIANGLES, Vertices, Entity.defaultTextureCoordinates)
   
   def toMesh(monster: Monster): IMesh = {
-    monster.mesh = new DefaultMesh(AnimationFrames(0), Geometry, Queue.TRANSPARENCY);
+    monster.mesh = new DefaultMesh(new ColorMapMaterial(AnimationFrames(0)), Geometry, Queue.TRANSPARENCY);
     monster.mesh.setPosition(monster.position.add(new Vec3(Width / 2f, Height / 2f, -0.5f)))
     monster.mesh
   }
@@ -97,13 +113,13 @@ object Monster {
     fixtureDef.density = 1;
         
     val leftSensor: PolygonShape = new PolygonShape
-    leftSensor.setAsBox(0.1f, Width - 0.1f, new org.jbox2d.common.Vec2(-Width / 2f, 0f), 0f)
+    leftSensor.setAsBox(0.1f, Height / 2f - 0.1f, new org.jbox2d.common.Vec2(-Width / 2f, 0f), 0f)
     val leftSensorFixtureDef: FixtureDef = new FixtureDef
     leftSensorFixtureDef.shape = leftSensor
     leftSensorFixtureDef.isSensor = true
     
     val rightSensor: PolygonShape = new PolygonShape
-    rightSensor.setAsBox(0.1f, Width - 0.1f, new org.jbox2d.common.Vec2(Width / 2f, 0f), 0f)
+    rightSensor.setAsBox(0.1f, Height / 2f - 0.1f, new org.jbox2d.common.Vec2(Width / 2f, 0f), 0f)
     val rightSensorFixtureDef: FixtureDef = new FixtureDef
     rightSensorFixtureDef.shape = leftSensor
     rightSensorFixtureDef.isSensor = true
