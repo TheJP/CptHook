a085362d0be4dad5ab1c585479bc5a89f3ec8f52
David Eichin <david@eich.in>
2016-01-13 09:44:14 +0100

    fixed monster positioning

--- CptHook/src/main/scala/ch/fhnw/cpthook/model/Monster.scala
+++ CptHook/src/main/scala/ch/fhnw/cpthook/model/Monster.scala
@@ -60,7 +60,7 @@ class Monster(var position: Position) extends Entity
   }
   
   def resetMeshPosition() {
-    mesh.setPosition(position add new Vec3(0.5f, -0.5f, 0))
+    mesh.setPosition(position add new Vec3(Monster.RealDimensions._1 / 2f, -Monster.RealDimensions._2 / 2f, 0))
     mesh.setTransform(Mat4.multiply(Mat4.rotate(-currentRotation.floatValue(), new Vec3(0, 1, 0)), mesh.getTransform))
   }
   
@@ -111,13 +111,13 @@ class Monster(var position: Position) extends Entity
 object Monster {
   
   val Speed = 2f
-  val Width = 0.9f
-  val Height = 0.9f
+  val Box2DDimensions = (0.9f, 0.9f)
+  val RealDimensions = (1.0f, 1.0f)
   val AnimationFrames = (1 to 11).map { n => Entity.loadTexture(s"../assets/monster_step$n.png") }.toArray
   val AnimationStepTime = 0.3f
   val RotationStep = 10
   
-  val Vertices = Entity.twoDimensionalPlane(Width, Height, 0f)
+  val Vertices = Entity.twoDimensionalPlane(Box2DDimensions._1, Box2DDimensions._2, 0f)
   val Geometry = DefaultGeometry.createVM(Primitive.TRIANGLES, Vertices, Entity.defaultTextureCoordinates)
   
   def toMesh(monster: Monster): IMesh = {
@@ -128,12 +128,12 @@ object Monster {
   
   def linkBox2d(monster: Monster, world: World): Unit = {
     val bodyDef = new BodyDef
-    bodyDef.position.set(monster.position.x, monster.position.y)
+    bodyDef.position.set(monster.position.x + RealDimensions._1 / 2.0f, monster.position.y - RealDimensions._2 / 2.0f)
     bodyDef.`type` = BodyType.DYNAMIC
     bodyDef.fixedRotation = true
     
     val shape: PolygonShape = new PolygonShape
-    shape.set(Player.createPlayerPolygon(Width  / 2f, Height / 2f, 0.1f), 8)
+    shape.set(Player.createPlayerPolygon(Box2DDimensions._1  / 2f, Box2DDimensions._2 / 2f, 0.1f), 8)
     val fixtureDef: FixtureDef = new FixtureDef
     fixtureDef.shape = shape
     fixtureDef.friction = 0.1f;        
@@ -141,13 +141,13 @@ object Monster {
     fixtureDef.density = 1;
         
     val leftSensor: PolygonShape = new PolygonShape
-    leftSensor.setAsBox(0.1f, Height / 2f - 0.1f, new org.jbox2d.common.Vec2(-Width / 2f, 0f), 0f)
+    leftSensor.setAsBox(0.1f, Box2DDimensions._2 / 2f - 0.1f, new org.jbox2d.common.Vec2(-Box2DDimensions._1 / 2f, 0f), 0f)
     val leftSensorFixtureDef: FixtureDef = new FixtureDef
     leftSensorFixtureDef.shape = leftSensor
     leftSensorFixtureDef.isSensor = true
     
     val rightSensor: PolygonShape = new PolygonShape
-    rightSensor.setAsBox(0.1f, Height / 2f - 0.1f, new org.jbox2d.common.Vec2(Width / 2f, 0f), 0f)
+    rightSensor.setAsBox(0.1f, Box2DDimensions._2 / 2f - 0.1f, new org.jbox2d.common.Vec2(Box2DDimensions._1 / 2f, 0f), 0f)
     val rightSensorFixtureDef: FixtureDef = new FixtureDef
     rightSensorFixtureDef.shape = rightSensor
     rightSensorFixtureDef.isSensor = true
