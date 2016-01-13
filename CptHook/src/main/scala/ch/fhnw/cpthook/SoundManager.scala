2bfd0e4cd9c150a51e83ace648ffa8c22640e980
David Eichin <david@eich.in>
2016-01-13 20:20:25 +0100

    sound v5

--- CptHook/src/main/scala/ch/fhnw/cpthook/SoundManager.scala
+++ CptHook/src/main/scala/ch/fhnw/cpthook/SoundManager.scala
@@ -1,45 +1,38 @@
 package ch.fhnw.cpthook
 
-import java.io.ByteArrayInputStream
-import java.net.URL
+import javax.sound.sampled.FloatControl
+import ch.fhnw.ether.audio.URLAudioSource
 import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.Paths
-import javax.sound.sampled.AudioInputStream
 import javax.sound.sampled.AudioSystem
+import javax.sound.sampled.DataLine
+import java.io.ByteArrayInputStream
+import scala.collection.mutable.ListBuffer
+import java.nio.file.Paths
+import java.util.concurrent.atomic.AtomicInteger
+import scala.collection.mutable.Stack
 import javax.sound.sampled.Clip
-import net.java.truecommons.io.Loan.loan
-import java.io.File
-import java.io.DataInputStream
-import java.io.FileInputStream
 import javax.sound.sampled.LineEvent
 import javax.sound.sampled.LineListener
-import java.util.concurrent.locks.Lock
-import java.util.concurrent.locks.ReentrantLock
-import scala.collection.mutable.MutableList
-import scala.collection.mutable.Map
-import javax.sound.sampled.FloatControl
-import javafx.scene.media.AudioClip
-import com.sun.media.jfxmediaimpl.AudioClipProvider
-import javafx.scene.media.Media
-import javafx.scene.media.MediaPlayer
-import javazoom.jl.player.Player
-import javazoom.jl.player.advanced.AdvancedPlayer
-import java.util.concurrent.atomic.AtomicBoolean
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
-import java.util.concurrent.atomic.AtomicInteger
-import ch.fhnw.ether.audio.URLAudioSource
-import javax.sound.sampled.DataLine
-import scala.collection.mutable.Stack
+import scala.collection.immutable.Queue
+
+/**
+ * PlaySound, StopSound and StopAll are commands that can be queued
+ */
+case class PlaySound(val sound: String, val gain: Float, val loop: Boolean, val stopOthers: Boolean)
+case class StopSound(val sound: String)
+case class StopAll()
+
+/**
+ * MappedClip is used to save the currently playing clips and keep a mapping to what sound it was
+ */
+case class MappedClip(val sound: String, val clip: Clip)
 
 /**
  * Object that manages, which sounds are currently played.
  */
 object SoundManager {
-  
-  val MaxSounds = 25
-  private val soundCount = new AtomicInteger(0)
+
+  val MaxSounds = 10
 
   val AmbientSound = "ambient"
   val BlockPlaceSound = "place"
@@ -48,7 +41,7 @@ object SoundManager {
   val BumpLoopSound = "bumploop"
   val JumpSound = "jump"
   val LevelSound = "level"
-  
+
   private val sounds = Map(
     AmbientSound -> loadSound("./sounds/ambient.mp3"),
     BlockPlaceSound -> loadSound("./sounds/blockplace.mp3"),
@@ -56,39 +49,29 @@ object SoundManager {
     BumpSound -> loadSound("./sounds/bump.mp3"),
     BumpLoopSound -> loadSound("./sounds/bumploop.mp3"),
     JumpSound -> loadSound("./sounds/jump.mp3"),
-    LevelSound -> loadSound("./sounds/level.mp3")
-  )
-  
-  private var clipsLock = new ReentrantLock()
-  private var clips: Map[String, MutableList[Clip]] = sounds.map { case (key, value) => (key, MutableList[Clip]()) }
-  private var clipCache: Map[String, Stack[Clip]] = sounds.map { case (key, value) => (key, Stack[Clip]()) }
-  
-  clipCache.keys.foreach { k => clipCache(k).push(getClip(k)) }
-  
+    LevelSound -> loadSound("./sounds/level.mp3"))
+
+  private var playing: List[MappedClip] = List()
+  private var cache: Map[String, Stack[Clip]] = sounds.map { case (key, value) => (key, Stack[Clip]()) }
+  private var queue: Queue[AnyRef] = Queue()
+
+  // preload
+  cache.keys.foreach { k => cache(k).push(createClip(k)) }
+
   private def loadSound(path: String): Array[Byte] = {
     try {
-       return Files.readAllBytes(Paths.get(getClass.getResource(path).toURI()))
+      return Files.readAllBytes(Paths.get(getClass.getResource(path).toURI()))
     } catch {
       case e: Throwable => e.printStackTrace()
     }
     return null
   }
-  
-  def getClip(sound: String): Clip = {
-    var clip: Clip = null
-    clipsLock.lock()
-    if (clipCache(sound).length > 0) {
-      clip = clipCache(sound).pop
-      clip.setFramePosition(0)
-      clip.loop(0)
-    } else {
-      val audio = URLAudioSource.getStream(new ByteArrayInputStream(sounds(sound)))
-      val info = new DataLine.Info(classOf[Clip], audio.getFormat())
-      clip = AudioSystem.getLine(info).asInstanceOf[Clip]
-      clip.open(audio)
-    }
-    clipsLock.unlock()
- 
+
+  private def createClip(sound: String): Clip = {
+    val audio = URLAudioSource.getStream(new ByteArrayInputStream(sounds(sound)))
+    val info = new DataLine.Info(classOf[Clip], audio.getFormat())
+    val clip = AudioSystem.getLine(info).asInstanceOf[Clip]
+    clip.open(audio)
     clip
   }
 
@@ -98,64 +81,93 @@ object SoundManager {
 
   def playSound(sound: String, gain: Float, loop: Boolean, stopOthers: Boolean): Unit = {
 
-    cleanUp()
-
-    var lastCount = soundCount.get
-    if (lastCount >= MaxSounds) {
-      println("sound limit reached")
-      return
+    this.synchronized {
+      queue = queue.enqueue(PlaySound(sound, gain, loop, stopOthers))
     }
 
-    while (!soundCount.compareAndSet(lastCount, lastCount + 1)) {
-      var lastCount = soundCount.get
-      if (lastCount >= MaxSounds) {
-        println("sound limit reached")
-        return
-      }
+  }
+
+  def stopSound(sound: String): Unit = {
+    this.synchronized {
+      queue = queue.enqueue(StopSound(sound))
+    }
+  }
+
+  def stopAll(): Unit = {
+    this.synchronized {
+      queue = queue.enqueue(StopAll())
+    }
+  }
+
+  private def playSound(c: PlaySound): Unit = {
+    
+    var clip: Clip = null
+    if (!cache(c.sound).isEmpty) {
+      clip = cache(c.sound).pop()
+      clip.setFramePosition(0)
+    } else {
+      clip = createClip(c.sound)
     }
     
-    val clip = getClip(sound)
-
-    clipsLock.lock()
-    clips(sound) += clip
-    clipsLock.unlock()
-
-    if (loop) {
+    if (c.loop) {
       clip.loop(Clip.LOOP_CONTINUOUSLY)
+    } else {
+      clip.loop(0)
     }
-
+    
+    if (c.stopOthers) {
+      stopSound(StopSound(c.sound))
+    }
+    
     val gainControl = clip.getControl(FloatControl.Type.MASTER_GAIN).asInstanceOf[FloatControl];
-    gainControl.setValue(toDb(gain))
-
+    gainControl.setValue(toDb(c.gain))
+    
+    playing ::= MappedClip(c.sound, clip)
+    
     clip.start()
   }
   
-  def toDb(gain: Float) = {
-    (Math.log(gain)/Math.log(10.0)*20.0).toFloat;
+  private def stopSound(c: StopSound): Unit = {
+    playing.filter(m => c.sound == m.sound).foreach(_.clip.stop())
   }
   
-  def cleanUp(): Unit = {
-    clipsLock.lock()
-    clips.keys.foreach {k => 
-      val done = clips(k).filter(!_.isRunning())
-      clips(k) = clips(k).filter(_.isRunning())
-      done.foreach {c =>
-        clipCache(k).push(c)
-        soundCount.decrementAndGet()
+  private def stopAll(c: StopAll  ): Unit = {
+    playing.foreach(_.clip.stop())
+  }
+  
+  def toDb(gain: Float) = {
+    (Math.log(gain) / Math.log(10.0) * 20.0).toFloat;
+  }
+
+  private val thread = new Thread(new Runnable() {
+    def run(): Unit = {
+      while (!Thread.interrupted()) {
+        var workingQueue: Queue[AnyRef] = null
+        this.synchronized {
+          workingQueue = queue
+          queue = Queue()
+        }
+        
+        playing.filter(!_.clip.isActive()).foreach { x =>
+          cache(x.sound).push(x.clip)
+        }
+        playing = playing.filter(_.clip.isActive())
+        
+        workingQueue.foreach { cmd =>
+          println(cmd)
+          cmd match {
+            case c: PlaySound if playing.length < MaxSounds => playSound(c)
+            case c: StopSound => stopSound(c)
+            case c: StopAll => stopAll(c)
+            case _ => println("overloaded")
+          }
+        }
+        
+        Thread.sleep(5)
       }
     }
-    clipsLock.unlock()
-  }
+  })
+  thread.setDaemon(true)
+  thread.start()
   
-  def stopSound(sound: String): Unit = {
-    clipsLock.lock()
-    clips(sound).foreach { _.stop() }
-    clipsLock.unlock()
-  }
-  
-  def stopAll(): Unit = {
-    clipsLock.lock()
-    clips.values.foreach { c => c.foreach { _.stop() } }
-    clipsLock.unlock()
-  }
 }
